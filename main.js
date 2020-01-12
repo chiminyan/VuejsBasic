80fc2f431c467bf8a04c80f0632bc89630f33bd4
Tomomi <sanctity3591@gmail.com>
2020-01-12 23:25:45 +0900

    section_23_b

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 Vue.component('comp-child', {
     // テンプレートでvalを使用
-    template: '<p>{{ val }}</p>',
+    template: '<p id="child" class="child">ChildComponent</p>',
     // 受け取る属性名を指定
     props: ['val']
 })
 var app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        message: 'hello'
+    }
 })
