3681fa22fa24b8b2ed2f76d4c866709720cc0405
Tomomi <sanctity3591@gmail.com>
2020-01-13 00:58:30 +0900

    section_23_d

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 Vue.component('comp-child', {
     // テンプレートでvalを使用
-    template: '<li>{{ name }} HP.{{ hp }}</li>',
+    template: '<li>{{ name }} HP.{{ hp }}\
+        <button v-on:click="doAttack">攻撃する</button></li>',
     // 受け取る属性名を指定
-    props: ['name', 'hp']
+    props: { id: Number, name: String, hp: Number },
+    methods: {
+        // ボタンのクリックイベントのハンドラから$emitでattackを発火する
+        doAttack: function() {
+            // 引数として自分のIDを渡す
+            this.$emit('attack', this.id)
+        }
+    }
 })
 var app = new Vue({
     el: '#app',
@@ -12,5 +20,16 @@ var app = new Vue({
             { id: 2, name: 'ゴブリン', hp: 200 },
             { id: 3, name: 'ドラゴン', hp: 500 }
         ]
+    },
+    methods: {
+        // attackが発生した！
+        handleAttack: function(id) {
+            // 引数のIDから要素を検索
+            var item = this.list.find(function(el) {
+                return el.id === id
+            })
+            // HPが0より多ければ10減らす
+            if (item !== undefined && item.hp > 0) item.hp -= 10
+        }
     }
 })
